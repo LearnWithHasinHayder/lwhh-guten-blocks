abef41401047beb211691671244c5030ac35deb7
obiPlabon <obi.plabon@gmail.com>
2019-01-21 19:12:43 +0600

    Added alert select block

---
+++ src/alert-select/helper.js
@@ -0,0 +1,36 @@
+const { __ } = wp.i18n;
+
+export const TYPES = [
+    {
+        value: 'primary',
+        label: __('Primary'),
+    },
+    {
+        value: 'secondary',
+        label: __('Secondary'),
+    },
+    {
+        value: 'success',
+        label: __('Success'),
+    },
+    {
+        value: 'danger',
+        label: __('Danger'),
+    },
+    {
+        value: 'warning',
+        label: __('Warning'),
+    },
+    {
+        value: 'info',
+        label: __('Info'),
+    },
+    {
+        value: 'light',
+        label: __('Light'),
+    },
+    {
+        value: 'dark',
+        label: __('Dark'),
+    }
+];
